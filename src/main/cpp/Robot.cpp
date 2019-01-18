8699f792645afaebbd5ce141744b571327f19fe0
Tigertronics <team2053tigertronics@gmail.com>
2019-01-18 17:34:34 -0500

    working on field positioning

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -31,6 +31,7 @@ void Robot::RobotInit() {
 	SmartDashboard::PutData("Zero Yaw", new ZeroYaw());
 	cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture("camera", "/dev/video0");
 	camera.SetResolution(320, 240);
+	
 }
 
 /**
