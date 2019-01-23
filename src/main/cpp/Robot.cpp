281a3d3873ffe8d214cbba2499f32d843c1fdc75
Tigertronics <team2053tigertronics@gmail.com>
2019-01-22 19:38:47 -0500

    fixed camera on dashboard

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -37,6 +37,7 @@ void Robot::RobotInit() {
 	SmartDashboard::PutData("Zero Pose", new ZeroPose());
 	cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture("camera", "/dev/video0");
 	camera.SetResolution(320, 240);
+	camera.SetConnectionStrategy(cs::VideoSource::ConnectionStrategy::kConnectionKeepOpen);
 	
 }
 
